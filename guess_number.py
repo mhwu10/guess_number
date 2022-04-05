12afca67d433395a7d16f4c6170bc9bfc5cf97f9
MH Wu <apple49755@gmail.com>
2022-04-05 18:07:41 +0100

    count

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,19 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 
 while True:
+    count += 1 # count = count + 1
     num = input('guess a number: ')
     num = int(num)
     if num == r:
         print('you get it!')
+        print('You have guessed', count, 'times')
         break
     elif num > r:
         print(num, 'is larger')
     else:
         print(num, 'is smaller')
+    print('You have guessed', count, 'times')
+    
